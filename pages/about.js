c9bf1468886cf16afdc4a5d60fb8ccc059db82da
Zi Nean Teoh <zinean00@gmail.com>
2022-04-02 18:14:00 -0500

    first migration

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+
+function about() {
+	return (
+		<div>
+			<Navbar />
+
+			<Footer />
+		</div>
+	);
+}
+
+export default about;
