26ee56c141c3d59eb9cc2cc0bb72d8fcbe81ce23
longjichen1 <chenjerry222@gmail.com>
2022-04-02 20:39:47 -0500

    index section one and public org files

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="bg-[#28315a] sticky flex justify-end shadow-sm ">
       <div className="inline-block mr-auto mt-3 ml-2 w-[40px] h-[40px]">
         <Link href="/">
-          <Image alt="Logo" src="/logo.png" width={100} height={100} />
+          <Image alt="Logo" src="/logos/logo.png" width={100} height={100} />
         </Link>
       </div>
       <div className="text-white m-[20px] hover:underline" id="about">
