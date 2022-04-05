6c258da373f916ae1813a97c6201861d7694b176
Zi Nean Teoh <zinean00@gmail.com>
2022-04-05 09:39:44 -0500

    implement footer, add logo, and fix navbar

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Image from "next/image";
+import FooterSquiggle from "../public/svgs/footer_squiggle.svg";
+import FacebookIcon from "./icons/FacebookIcon";
+import InstagramIcon from "./icons/InstagramIcon";
+import TwitchIcon from "./icons/TwitchIcon";
+import YoutubeIcon from "./icons/YoutubeIcon";
+import GithubIcon from "./icons/GithubIcon";
+import TwitterIcon from "./icons/TwitterIcon";
 
 const Footer = () => {
-	return <div>footer</div>;
+	return (
+		<div className="w-full mt-10">
+			<FooterSquiggle className="mb-[-5px]" />
+			<div className="flex flex-row-reverse md:flex-row justify-center content-center md:justify-between px-[2rem] pt-[2rem] pb-[3rem] text-white bg-[#28315A]">
+				<div className="flex flex-col">
+					<p className="text-[3rem]">Contact Us</p>
+					<p>info [at] vandyhacks.org</p>
+					<p>sponsorship [at] vandyhacks.org</p>
+					<div className="flex flex-row pt-3">
+						<FacebookIcon />
+						<InstagramIcon />
+						<TwitchIcon />
+						<YoutubeIcon />
+						<GithubIcon />
+						<TwitterIcon />
+					</div>
+				</div>
+				<div className="flex flex-col justify-center items-center pr-5">
+					<Image
+						src="/logos/medium_logo.png"
+						alt="Logo"
+						width={150}
+						height={140}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,25 +3,31 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <div className="bg-[#28315a] sticky flex justify-end shadow-sm ">
-      <div className="inline-block mr-auto mt-3 ml-2 w-[40px] h-[40px]">
-        <Link href="/">
-          <Image alt="Logo" src="/logos/logo.png" width={100} height={100} />
-        </Link>
-      </div>
-      <div className="text-white m-[20px] hover:underline" id="about">
-        <Link href="/about">
-          <a>About Us</a>
-        </Link>
-      </div>
-      <div className="text-white m-[20px] hover:underline" id="events">
-        <Link href="/events">
-          <a>Events</a>
-        </Link>
-      </div>
-    </div>
-  );
+	// TODO Make text color #FBF1BC and underlined when selected
+	return (
+		<div className="bg-[#28315a] sticky flex justify-end shadow-sm text-xl px-10">
+			<div className="inline-block mr-auto mt-3 w-[40px] h-[40px]">
+				<Link href="/">
+					<Image
+						alt="Logo"
+						src="/logos/small_logo.png"
+						width={100}
+						height={100}
+					/>
+				</Link>
+			</div>
+			<div className="text-white m-[20px] hover:underline" id="about">
+				<Link href="/about">
+					<a>About</a>
+				</Link>
+			</div>
+			<div className="text-white m-[20px] hover:underline" id="events">
+				<Link href="/events">
+					<a>Events</a>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
