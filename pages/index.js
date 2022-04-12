56ef4264c4ac64b21e41f33c95c52d7817952cf5
longjichen1 <chenjerry222@gmail.com>
2022-04-11 22:30:13 -0500

    index fixed mobile

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,9 @@ const Home = () => {
       <Navbar />
 
       {/* Logo */}
-      <div className="h-[60vh] md:h-[80vh] md:mt-0  bg-center  bg-[length:720px_500px] sm:bg-[length:1920px_100%] bg-[url('/svgs/landing_section_one_mobile.svg')] sm:bg-[url('/svgs/landing_section_one.svg')]">
-        <div className="mx-auto h-[280px] w-[300px] pt-24 md:w-[342px] md:h-[285px] md:pt-32">
+      <div className="mb-[-45.7px] p-0 bg-[#b0cee3] text-[#b0cee3]">h</div>
+      <div className="h-[60vh] md:h-[80vh] md:mt-0  bg-center bg-[length:720px_500px] sm:bg-[length:1920px_100%] xl:bg-[length:100%] bg-[url('/svgs/landing_section_one_mobile.svg')] sm:bg-[url('/svgs/landing_section_one.svg')]">
+        <div className="mx-auto h-[280px] w-[300px] pt-28 md:w-[342px] md:h-[285px] 3xl:pt-[20vh] 3xl:w-[684px] 3xl:h-[570px] md:pt-32">
           <Image src="/logos/full_logo.png" width={684} height={570} />
         </div>
       </div>
@@ -25,10 +26,10 @@ const Home = () => {
       <MobileHappeningNow className="absolute sm:hidden" />
       <BottomSBlue className="-z-10 absolute mt-[-200px] w-full  sm:hidden" />
 
-      <div className="mt-10 pt-0 bg-white">
+      <div className="sm:mt-0 mt-72 pt-1 bg-white">
         {/* box here with mobile stuff load if resolution is a certain size */}
 
-        <div className="mt-72 sm:mt-0 font-semibold bg-no-repeat bg-center text-center bg-[url('/svgs/yellow_title.svg')]  leading-4 h-16 text-3xl">
+        <div className="mt-12 sm:mt-0 font-semibold bg-no-repeat bg-center text-center bg-[url('/svgs/yellow_title.svg')]  leading-4 h-16 text-3xl">
           Happening Now
         </div>
         <div className="block sm:w-[50%] w-0 m-auto">
