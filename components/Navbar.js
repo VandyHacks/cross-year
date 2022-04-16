d63c67a0ad143d464e86140729d832fce6f170a3
Zi Nean Teoh <zinean00@gmail.com>
2022-04-15 19:23:01 -0500

    make navbar sticky on top

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,38 +4,38 @@ import Link from "next/link";
 import CollapsingNav from "./CollapsingNav";
 
 const Navbar = () => {
-  // TODO Make text color #FBF1BC and underlined when selected
-  return (
-    <div className="bg-[#28315a] border-b-2 border-black drop-shadow-xl filter shadow-md sticky flex justify-end text-xl px-3  font-semibold text-white">
-      <div className="inline-block mr-auto mt-3 w-[40px] h-[40px]">
-        <Link href="/">
-          <Image
-            alt="Logo"
-            src="/logos/small_logo.png"
-            width={100}
-            height={100}
-          />
-        </Link>
-      </div>
-      <div
-        className="invisible sm:visible text-base m-[20px] hover:underline"
-        id="about"
-      >
-        <Link href="/about">
-          <a>About Us</a>
-        </Link>
-      </div>
-      <div
-        className="invisible sm:visible text-base m-[20px] hover:underline"
-        id="events"
-      >
-        <Link href="/events">
-          <a>Events</a>
-        </Link>
-      </div>
-      <CollapsingNav />
-    </div>
-  );
+	// TODO Make text color #FBF1BC and underlined when selected
+	return (
+		<div className="sticky top-0 z-50 bg-[#28315a] border-b-2 border-black drop-shadow-xl filter shadow-md sticky flex justify-end text-xl px-3  font-semibold text-white">
+			<div className="inline-block mr-auto mt-3 w-[40px] h-[40px]">
+				<Link href="/">
+					<Image
+						alt="Logo"
+						src="/logos/small_logo.png"
+						width={100}
+						height={100}
+					/>
+				</Link>
+			</div>
+			<div
+				className="invisible sm:visible text-base m-[20px] hover:underline"
+				id="about"
+			>
+				<Link href="/about">
+					<a>About Us</a>
+				</Link>
+			</div>
+			<div
+				className="invisible sm:visible text-base m-[20px] hover:underline"
+				id="events"
+			>
+				<Link href="/events">
+					<a>Events</a>
+				</Link>
+			</div>
+			<CollapsingNav />
+		</div>
+	);
 };
 
 export default Navbar;
