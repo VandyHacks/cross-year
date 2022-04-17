aad92f164edd1531edc9b18cd5b247fea8f1689b
Sneh Patel <patelsneh21@yahoo.com>
2022-04-16 20:54:42 -0500

    Trying to fix netlify form submission

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,19 +1,31 @@
 const { Client } = require("@notionhq/client");
 exports.handler = async function (event) {
     const form = JSON.parse(event.body).payload.data;
-    const notion = new Client({ auth: "secret_GKrDwBeqQrVkc85joZPwl6CG1s1usCAixKmB2O3TMZK" });
+    const notion = new Client({ auth: "secret_GKrDwBeqQrVkc85joZPwl6CG1s1usCAixKmB2O3TMZK" }); //Replace this with integration secret token
     try {
       const new_row = {
         parent: {
-          database_id: "9cf26403319449199bfff37c0d99a3ec",
+          database_id: "9cf26403319449199bfff37c0d99a3ec", //Replace this with the database id (found in url of notion page)
         },
         properties: {
-          Email: {
-            email: form.email
-          },
-          Date: {
-            content: Date.now(),
-          },
+            Email: {
+                title: [
+                  {
+                    text: {
+                      content: form.email
+                    },
+                  },
+                ],
+              },
+              Date: {
+                rich_text: [
+                  {
+                    text: {
+                      content: Date.now()
+                    },
+                  },
+                ],
+              }
         },
       };
       await notion.pages.create(new_row);
