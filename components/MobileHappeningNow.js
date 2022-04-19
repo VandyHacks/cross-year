3cb6efa886e5be6839345dc4c1a9f51bad0168c5
Zi Nean Teoh <zinean00@gmail.com>
2022-04-19 16:47:14 -0500

    merge changes

--- components/MobileHappeningNow.js
+++ components/MobileHappeningNow.js
@@ -1,7 +1,7 @@
 import React from "react";
 export const MobileHappeningNow = () => {
 	return (
-		<div className="sm:hidden -z-10 absolute top-[40%] w-full h-[600px] bg-cover bg-no-repeat bg-[url('/misc/workshop.png')]">
+		<div className="sm:hidden -z-10 absolute top-[40%] w-full h-[600px] bg-cover bg-no-repeat bg-[url('/misc/workshop2.png')]">
 			<div className="absolute h-[200px] bg-white -mt-52"></div>
 		</div>
 	);
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,74 +1,75 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import CollapsingNav from "./CollapsingNav";
 
 const Navbar = () => {
-  // TODO Make text color #FBF1BC and underlined when selected
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <>
-      <div className="sticky top-0 z-50 bg-[#28315a] border-b-2 border-black drop-shadow-xl filter shadow-md flex justify-end text-xl px-3  font-semibold text-white">
-        <div className="inline-block ml-[8px] sm:ml-[40px] mr-auto mt-3 w-[40px] h-[40px]">
-          <Link href="/">
-            <Image
-              className="hover:cursor-pointer"
-              alt="Logo"
-              src="/logos/small_logo.png"
-              width={100}
-              height={100}
-            />
-          </Link>
-        </div>
-        <div
-          className="invisible sm:visible text-base m-[20px] hover:underline"
-          id="about"
-        >
-          <Link href="/about">
-            <a>About Us</a>
-          </Link>
-        </div>
-        <div
-          className="invisible sm:visible text-base m-[20px] hover:underline"
-          id="events"
-        >
-          <Link href="/events">
-            <a>Events</a>
-          </Link>
-        </div>
-        <CollapsingNav setIsOpen={setIsOpen} isOpen={isOpen} />
-      </div>
-      {isOpen ? (
-        <div className="z-40 fixed w-full top-16 bg-[#28315a]">
-          <div className="md:hidden" id="mobile-menu">
-            <a
-              href="/"
-              className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
-            >
-              {" "}
-              Home{" "}
-            </a>
-            <a
-              href="/about"
-              className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
-            >
-              {" "}
-              About{" "}
-            </a>
-            <a
-              href="/events"
-              className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
-            >
-              {" "}
-              Events{" "}
-            </a>
-          </div>
-        </div>
-      ) : (
-        <div></div>
-      )}
-    </>
-  );
+	// TODO Make text color #FBF1BC and underlined when selected
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<>
+			<div className="sticky top-0 z-50 bg-[#28315a] border-b-2 border-black drop-shadow-xl filter shadow-md flex justify-end text-xl px-3  font-semibold text-white">
+				<div className="inline-block ml-[8px] sm:ml-[40px] mr-auto mt-3 w-[40px] h-[40px]">
+					<Link href="/">
+						<Image
+							className="hover:cursor-pointer"
+							alt="Logo"
+							src="/logos/small_logo.png"
+							width={100}
+							height={100}
+						/>
+					</Link>
+				</div>
+				<div
+					className="invisible sm:visible text-base m-[20px] hover:underline"
+					id="about"
+				>
+					<Link href="/about">
+						<a>About Us</a>
+					</Link>
+				</div>
+				<div
+					className="invisible sm:visible text-base m-[20px] hover:underline"
+					id="events"
+				>
+					<Link href="/events">
+						<a>Events</a>
+					</Link>
+				</div>
+				<CollapsingNav setIsOpen={setIsOpen} isOpen={isOpen} />
+			</div>
+			{isOpen ? (
+				<div className="z-40 fixed w-full top-16 bg-[#28315a]">
+					<div className="md:hidden" id="mobile-menu">
+						<a
+							href="/"
+							className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
+						>
+							{" "}
+							Home{" "}
+						</a>
+						<a
+							href="/about"
+							className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
+						>
+							{" "}
+							About{" "}
+						</a>
+						<a
+							href="/events"
+							className="text-center text-white hover:bg-[#37406c]  block px-3 py-2 rounded-md text-base font-medium"
+						>
+							{" "}
+							Events{" "}
+						</a>
+					</div>
+				</div>
+			) : (
+				<div></div>
+			)}
+		</>
+	);
 };
 
 export default Navbar;
