1af419c8f9817fded65675d27ccdda4c7d13650b
longjichen1 <chenjerry222@gmail.com>
2022-04-19 17:15:14 -0500

    workshops

---
+++ pages/hackathons.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import AboutEgg from "../public/svgs/about_egg.svg";
+import TopBar from "../public/svgs/top_bar_header.svg";
+import EventShowcase from "../components/EventShowcase";
+
+const events = () => {
+  return (
+    <div className="bg-[url('/misc/leaves.png')]  bg-no-repeat bg-cover text-[#28315A]">
+      <Navbar />
+
+      <div className="mt-12 h-full text-center">
+        <div className="text-6xl py-5">Past Hackathons</div>
+        <div className="flex flex-col md:flex-row justify-center items-center w-full md:w-[80%] mx-auto pt-10"></div>
+        <div className="text-2xl">
+          Swipe below to see our past hackathon websites!
+        </div>
+        <EventShowcase />
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default events;
