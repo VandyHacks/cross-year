96b30b45939eed58b293efa9c5d279f3d664289f
longjichen1 <chenjerry222@gmail.com>
2022-04-19 17:01:57 -0500

    index

--- components/MobileHappeningNow.js
+++ components/MobileHappeningNow.js
@@ -1,8 +1,11 @@
 import React from "react";
 export const MobileHappeningNow = () => {
-	return (
-		<div className="sm:hidden -z-10 absolute top-[40%] w-full h-[600px] bg-cover bg-no-repeat bg-[url('/misc/workshop2.png')]">
-			<div className="absolute h-[200px] bg-white -mt-52"></div>
-		</div>
-	);
+  return (
+    <>
+      <div className="sm:hidden -z-10 absolute top-[40%] mb-16 w-full h-[600px] bg-cover bg-no-repeat bg-[url('/misc/workshop2.png')]">
+        {/* <div className="absolute h-[200px] bg-white -mt-52 "></div> */}
+      </div>
+      <div className="mb-16 text-transparent">d</div>
+    </>
+  );
 };
