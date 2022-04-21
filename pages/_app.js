cdb8b8b7c18bee0efca9dc7705a159a9853abc6a
Zi Nean Teoh <zinean00@gmail.com>
2022-04-21 12:00:25 -0500

    add favicon and fix some bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>VandyHacks</title>
+				<meta
+					name="VandyHacks"
+					content="Main website for VandyHacks, an annual hackathon hosted at Vanderbilt University."
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/favicon/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/favicon/safari-pinned-tab.svg"
+					color="#5bbad5"
+				/>
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
