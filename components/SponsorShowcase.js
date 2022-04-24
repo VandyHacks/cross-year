993abc7346aa72666bd5b3312256aa5347138dac
wangaustin <austin.wang220@gmail.com>
2022-04-24 13:53:06 -0500

    sponsors now link to careers, layout now sm:2, md:3, lg:4

--- components/SponsorShowcase.js
+++ components/SponsorShowcase.js
@@ -3,7 +3,7 @@ import { SponsorCard } from "./SponsorCard";
 import sponsorInfoList from "../data/sponsor_info.json";
 export const SponsorShowcase = () => {
 	return (
-        <div className="gap-x-8 gap-y-8 w-[90vw] md:w-[70vw] mx-auto flex-auto h-100 justify-items-center items-center pt-2 grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 ">
+        <div className="gap-x-8 gap-y-8 w-[90vw] md:w-[70vw] mx-auto flex-auto h-100 justify-items-center items-center pt-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{sponsorInfoList.map((sponsor, index) => (
 				<SponsorCard key={index} sponsorData={sponsor} />
 			))}
